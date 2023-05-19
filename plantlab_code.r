17d5ee2df7be5c46532b69e64d0efff34a6f37b1
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-05-19 17:41:42 +0200

    Create plantlab_code.r

---
+++ plantlab_code.r
@@ -0,0 +1,48 @@
+# ecological data on the pollination of flowering plants 
+
+install.packages("readxl")
+library(readxl)
+
+# load data from excel
+lamium <- read_excel("poll_lim.xlsx")
+summary(lamium)
+
+
+# calculate ratio (actual number of seeds/max nr of seeds that this species can produce) to make plots easier to interpret
+Ratio <- lamium$Success/4
+Ratio
+
+install.packages("multcomp")
+library(multcomp)
+
+# create a boxplot for visualization: 
+
+
+pdf("Seed_ratio.pdf",
+    width = 8, height = 7, # Width and height in inches
+    bg = "white",          # Background color
+    paper = "A4")
+plot_ratio <- boxplot((Ratio[lamium$Test == "PS"]), (Ratio[lamium$Test == "N"]), 
+                      names = c("Pollen Supplementation", "Natural"), 
+                      main = "Seeds/Tot Seeds")
+dev.off()
+
+# we create a general linear model to compare seed development success/failure
+model <- glm(cbind(lamium$Success, Failure) ~ lamium$Test, family = binomial, data = lamium)
+summary(model)
+
+ods <- model$deviance/model$df.residual
+ods # 3.23 this is to big so adjust family to "quasibinomial"
+# if >1 than the model is overdispersed so we need to adjust
+
+model2 <- glm(cbind(lamium$Success, Failure) ~ lamium$Test, family = quasibinomial, data = lamium)
+ods2 <- model2$deviance/model2$df.residual
+ods2 # still 2.796?? shouldn't it be smaller?
+
+
+model3 <- glm(cbind(Seeds, Ovule_aborted) ~ Test, data = lamium, family = binomial)
+summary(model3)# use posthoc test to define between which groups differences are found, 
+
+install.packages("emmeans")
+library(emmeans)
+pairs(emmeans(model2, ~ Test)) # since we only have 2 obviously the differences are found between these
