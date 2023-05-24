fde7b1273c6a8b5135b74a006b93cdd2f5465b55
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-05-24 12:09:54 +0200

    plantlab_code.r

--- plantlab_code.r
+++ plantlab_code.r
@@ -4,13 +4,19 @@ install.packages("readxl")
 library(readxl)
 
 # load data from excel
-lamium <- read_excel("C:/RStudio/plantlab/poll_lim.xlsx")
+lamium <- read_excel("poll_lim.xlsx")
 summary(lamium)
 
+# divide the dataset into the three patches
+group2 <- lamium[1:16,]
+group3 <- lamium[17:36,]
+group4 <- lamium[37:55,]
 
 # calculate ratio (actual number of seeds/max nr of seeds that this species can produce) to make plots easier to interpret
 Ratio <- lamium$Success/4
-Ratio
+Ratio2 <- group2$Success/4
+Ratio3 <- group3$Success/4
+Ratio4 <- group4$Success/4
 
 install.packages("multcomp")
 library(multcomp)
@@ -25,6 +31,26 @@ plot_ratio <- boxplot((Ratio[lamium$Test == "PS"]), (Ratio[lamium$Test == "N"]),
                       main = "Seeds/Tot Seeds")
 dev.off()
 
+# create boxplots for each of the patches
+pdf("Seed_ratio_per_patch.pdf",
+    width = 6, height = 10, # Width and height in inches
+    bg = "white",          # Background color
+    paper = "A4")
+par(mfrow = c(3, 1))
+plot_2 <- boxplot((Ratio2[group2$Test == "PS"]), (Ratio2[group2$Test == "N"]), 
+                      names = c("Pollen Supplementation", "Natural"), 
+                      main = "Seeds/Tot Seeds in Patch 2")
+
+plot_3 <- boxplot((Ratio3[group3$Test == "PS"]), (Ratio3[group3$Test == "N"]), 
+                  names = c("Pollen Supplementation", "Natural"), 
+                  main = "Seeds/Tot Seeds in Patch 3")
+
+plot_4 <- boxplot((Ratio4[group4$Test == "PS"]), (Ratio4[group4$Test == "N"]), 
+                  names = c("Pollen Supplementation", "Natural"), 
+                  main = "Seeds/Tot Seeds in Patch 4")
+dev.off()
+
+# calculate a t-test to see wether or not there are significant differences between the two treatments
 t.test(Ratio[lamium$Test == "PS"], Ratio[lamium$Test == "N"])
 # p-value is significant with 0.02, therfore the alternative hypothesis is accepted, the two are signficantly different
 
