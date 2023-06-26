7fc16a1997c2437a0f8a4ebb8bede1ecfa76fa1e
Lola Neuert <114755755+lolaneuert@users.noreply.github.com>
2023-06-26 15:38:10 +0200

    Update plantlab_code.r

--- plantlab_code.r
+++ plantlab_code.r
@@ -1,7 +1,10 @@
 # ecological data on the pollination of flowering plants 
-
-install.packages("readxl")
+# load necessary libraries
 library(readxl)
+library(multcomp)
+library(ggplot2)
+library(viridis)
+library(patchwork)
 
 # load data from excel
 lamium <- read_excel("poll_lim.xlsx")
@@ -18,46 +21,42 @@ Ratio2 <- group2$Success/4
 Ratio3 <- group3$Success/4
 Ratio4 <- group4$Success/4
 
-install.packages("multcomp")
-library(multcomp)
 
 # create a boxplot for visualization: 
-library(ggplot2)
-library(viridis)
-
 boxplot_ratio <- ggplot() +
   geom_boxplot(aes(x = lamium$Test, y  = Ratio, fill = lamium$Test)) + 
   labs(title = "Ratio between Seeds and Total Seeds", 
      subtitle = "Comparison between natural state and pollen supplementation", 
-     x = "Treatment", y = "Ratio") +
-   scale_fill_viridis(discrete = T, alpha = 0.4)
+     x = "Treatment", y = "Ratio", fill = "Treatment" ) +
+   scale_fill_viridis(discrete = T, alpha = 0.7)
 
-ggsave(filename = "boxplot_ratio.png", plot = boxplot_ratio, width = 7, height = 7)
+ggsave(filename = "boxplot_ratio.png", plot = boxplot_ratio, width = 5, height = 5)
 
 # create boxplots for each of the patches
 boxplot_2 <- ggplot() +
   geom_boxplot(aes(x = group2$Test, y  = Ratio2, fill = group2$Test)) + 
-  labs(title = "Ratio between Seeds and Total Seeds in Patch 2", 
-       subtitle = "Comparison between natural state and pollen supplementation", 
-       x = "Treatment", y = "Ratio") +
-  scale_fill_viridis(discrete = T, alpha = 0.4)
+  labs(title = "Ratio in Patch 2", 
+       subtitle = "Comparison between N and PS", 
+       x = "Treatment", y = "Ratio", fill = "Treatment") +
+  scale_fill_viridis(discrete = T, alpha = 0.7)
 
 boxplot_3 <- ggplot() +
   geom_boxplot(aes(x = group3$Test, y  = Ratio3, fill = group3$Test)) + 
-  labs(title = "Ratio between Seeds and Total Seeds in Patch 3", 
-       subtitle = "Comparison between natural state and pollen supplementation", 
-       x = "Treatment", y = "Ratio") +
-  scale_fill_viridis(discrete = T, alpha = 0.4)
+  labs(title = "Ratio in Patch 3", 
+       subtitle = "Comparison between N and PS", 
+       x = "Treatment", y = "Ratio", fill = "Treatment") +
+  scale_fill_viridis(discrete = T, alpha = 0.7)
 
 boxplot_4 <- ggplot() +
   geom_boxplot(aes(x = group4$Test, y  = Ratio4, fill = group4$Test)) + 
-  labs(title = "Ratio between Seeds and Total Seeds in Patch 4", 
-       subtitle = "Comparison between natural state and pollen supplementation", 
-       x = "Treatment", y = "Ratio") +
-  scale_fill_viridis(discrete = T, alpha = 0.4)
+  labs(title = "Ratio in Patch 4", 
+       subtitle = "Comparison between N and PS", 
+       x = "Treatment", y = "Ratio", fill = "Treatment") +
+  scale_fill_viridis(discrete = T, alpha = 0.7)
 
-patch_plots <- boxplot_2 / boxplot_3 / boxplot_4
-ggsave(filename = "boxplot_patches.png", plot = patch_plots, width = 7, height = 12)
+# add all boxplots together and download
+patch_plots <- boxplot_2 + boxplot_3 + boxplot_4
+ggsave(filename = "boxplot_patches.png", plot = patch_plots, width = 12, height = 6)
 
 # calculate a t-test to see wether or not there are significant differences between the two treatments
 t.test(Ratio[lamium$Test == "PS"], Ratio[lamium$Test == "N"])
@@ -71,9 +70,11 @@ ods <- model$deviance/model$df.residual
 ods # 3.23 this is to big so adjust family to "quasibinomial"
 # if >1 than the model is overdispersed so we need to adjust
 
+# redo the model
 model2 <- glm(cbind(lamium$Success, Failure) ~ lamium$Test, family = quasibinomial, data = lamium)
 ods2 <- model2$deviance/model2$df.residual
-ods2 # still3.23, shouldn't it be smaller now that we adjusted it to quasibinomial?
+ods2 
+summary(model2)
 
 # use posthoc test to define between which groups differences are found, 
 install.packages("emmeans")
